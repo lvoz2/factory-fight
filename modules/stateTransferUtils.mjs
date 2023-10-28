7aeeab809d6c0bed90d2010e338bafeadc9bd19c
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-10-28 15:26:24 +1100

    Create stateTransferUtils.mjs

---
+++ modules/stateTransferUtils.mjs
@@ -0,0 +1,16 @@
+export function save(name, state) {
+	const stateJSON = JSON.stringify(state);
+	localStorage.setItem(name, stateJSON);
+}
+
+export function load(name) {
+	const state = JSON.parse(localStorage.getItem(name));
+	return [name, state];
+}
+
+export function send(state) {
+	// TODO: Send state to server and parse response.
+}
+
+const package = {save, load, send};
+export default package;
