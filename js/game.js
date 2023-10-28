ba19ec824480c23df28ff9000e9339eae1d03f59
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-10-28 14:23:53 +1100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,15 @@
-
+$(function () {
+	const fileList = [];
+	for (file of fileList) {
+		let elem = "";
+		const type = file.slice(file.lastIndexOf("."));
+		switch (type) {
+			case ".css":
+				elem = "<link rel=\"stylesheet\" href=\"" + file "\"></link>";
+			case ".js":
+				elem = "<script src=\"" + file "\"></script>";
+			case ".mjs":
+				elem = "<script src=\"" + file "\" type=\"module\"></script";
+		$("head").append(elem);
+	}
+});
