75642d8b4cf73dba45a01413af4d2b9ea516fde1
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-11-01 09:51:52 +1100

    Update mapManipulation.mjs

--- js/mapManipulation.mjs
+++ js/mapManipulation.mjs
@@ -4,7 +4,7 @@ function generate(x, y) {
 	for (let curY = 0; curY < y; curY++) {
 		map.push([]);
 		for (let curX = 0; curX < x; curX++) {
-			const perlinVal = perlin.get(curX, curY) + 1;
+			const perlinVal = perlin.get(curX, curY);
 			console.log(perlinVal);
 			const texture = Math.floor(Math.random() * 5) + (perlinVal * 5);
 			map[curY].push(texture);
