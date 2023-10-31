8c90cde3cf1162a54e7117d3b6cc5f1a5df646b4
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-11-01 10:13:44 +1100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 $(function () {
-	const fileList = ["js/stateTransferUtils.mjs", "https://joeiddon.github.io/perlin/perlin.js", "js/mapManipulation.mjs", "js/noise.js"];
+	const fileList = ["js/noise.js", "js/stateTransferUtils.mjs", "js/mapManipulation.mjs"];
 	appendChunks(fileList)
 });
 
