a7173cb7ce692ef96691bf789a5925056372430a
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-11-01 09:37:21 +1100

    Create mapManipulation.mjs

---
+++ js/mapManipulation.mjs
@@ -0,0 +1,16 @@
+function generate(x, y) {
+	perlin.seed();
+	let map = [];
+	for (let curY = 0; curY < y; curY++) {
+		map.push([]);
+		for (let curX = 0; curX < x; curX++) {
+			const perlinVal = perlin.get(curX, curY) + 1;
+			const texture = Math.floor(Math.random() * 5) + (perlinVal * 5);
+			map[curY.push(texture);
+		}
+	}
+	return map;
+}
+
+const pkg = {generate};
+export default pkg;
