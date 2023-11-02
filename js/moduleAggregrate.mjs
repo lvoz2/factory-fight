2c44a2dc9511c3fb8cc91c0a4e003417866b1d20
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-11-02 17:42:52 +1100

    Create moduleAggregrate.mjs

---
+++ js/moduleAggregrate.mjs
@@ -0,0 +1,5 @@
+import stateUtils from "https://lvoz2.github.io/pvp-factory/js/stateTransferUtils.mjs";
+import mapManipulation from "https://lvoz2.github.io/pvp-factory/js/mapManipulation.mjs";
+
+const pkg = {stateUtils, mapManipulation};
+export default pkg;
