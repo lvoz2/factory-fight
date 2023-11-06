bebf9ecaee6e899c7674ba74460bf0bffe768996
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2023-11-07 10:48:20 +1100

    Update moduleAggregate.mjs

--- js/moduleAggregate.mjs
+++ js/moduleAggregate.mjs
@@ -2,13 +2,5 @@ import stateUtils from "https://lvoz2.github.io/factory-fight/js/stateTransferUt
 import mapManipulation from "https://lvoz2.github.io/factory-fight/js/mapManipulation.mjs";
 import menu from "https://lvoz2.github.io/factory-fight/js/menu.mjs";
 
-function returnContents(obj) {
-	let res = []
-	for (const key in obj) {
-		res.push(obj[key]);
-	}
-	return res;
-}
-
-const pkg = {menu, stateUtils, mapManipulation, ...returnContents(stateUtils), ...returnContents(mapManipulation), ...returnContents(menu)};
+const pkg = {menu, stateUtils, mapManipulation, ...stateUtils, ...mapManipulation, ...menu};
 export default pkg;
